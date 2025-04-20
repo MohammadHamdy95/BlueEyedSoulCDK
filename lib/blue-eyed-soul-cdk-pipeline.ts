735bc8cb349a4e56f918d4d83d359dba0583b835
Cossette Perez <cperezcomp1@student.rccd.edu>
2025-04-20 00:00:31 -0700

    adding manual approval for prod

--- lib/blue-eyed-soul-cdk-pipeline.ts
+++ lib/blue-eyed-soul-cdk-pipeline.ts
@@ -41,6 +41,51 @@ constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')
     );
 
+
+    const betaUploaderProject = new codebuild.PipelineProject(this, 'BetaUploaderProject', {
+        environment: {
+            buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+            environmentVariables: {
+                S3_BUCKET: { value: 'blue-eyed-soul-lambda-code-beta' },
+            },
+        },
+        buildSpec: codebuild.BuildSpec.fromObject({
+            version: '0.2',
+            phases: {
+                build: {
+                    commands: [
+                        'echo "Uploading lambda-deploy.zip to Beta bucket..."',
+                        'TIMESTAMP=$(date +%Y%m%d%H%M%S)',
+                        'S3_KEY=lambda-$TIMESTAMP.zip',
+                        'aws s3 cp lambda-deploy.zip s3://$S3_BUCKET/$S3_KEY',
+                        'echo "AWSTemplateFormatVersion: \'2010-09-09\'" > lambda.yml',
+                        'echo "Parameters:" >> lambda.yml',
+                        'echo "  LambdaFunctionName:" >> lambda.yml',
+                        'echo "    Type: String" >> lambda.yml',
+                        'echo "  LambdaFunctionCodeBucketName:" >> lambda.yml',
+                        'echo "    Type: String" >> lambda.yml',
+                        'echo "Resources:" >> lambda.yml',
+                        'echo "  MyLambdaFunction:" >> lambda.yml',
+                        'echo "    Type: AWS::Lambda::Function" >> lambda.yml',
+                        'echo "    Properties:" >> lambda.yml',
+                        'echo "      FunctionName: !Ref LambdaFunctionName" >> lambda.yml',
+                        'echo "      Handler: com.mo.blueeyedsoul.Handler::handleRequest" >> lambda.yml',
+                        'echo "      Runtime: java21" >> lambda.yml',
+                        'echo "      Role: arn:aws:iam::276366037431:role/service-role/profiles-update-beta-role-6br7r4r0" >> lambda.yml',
+                        'echo "      Timeout: 60" >> lambda.yml',
+                        'echo "      MemorySize: 512" >> lambda.yml',
+                        'echo "      Code:" >> lambda.yml',
+                        'echo "        S3Bucket: $S3_BUCKET" >> lambda.yml',
+                        'echo "        S3Key: $S3_KEY" >> lambda.yml',
+                    ],
+                },
+            },
+            artifacts: {
+                files: ['lambda.yml'],
+            },
+        }),
+    });
+
     const lambdaCodeBucket = s3.Bucket.fromBucketName(this, 'TargetLambdaCodeBucket', 'blue-eyed-soul-lambda-code');
     lambdaCodeBucket.grantPut(buildProject.role!);
 
@@ -53,6 +98,22 @@ constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       runOrder: 2,
     });
 
+    const uploadAction = new cp_actions.CodeBuildAction({
+        actionName: 'UploadLambdaZip',
+        project: betaUploaderProject,
+        input: buildOutput,
+        outputs: [new codepipeline.Artifact('BetaTemplate')],
+        runOrder: 1,
+    });
+
+    const uploadActionProd = new cp_actions.CodeBuildAction({
+        actionName: 'UploadLambdaZip',
+        project: betaUploaderProject,
+        input: buildOutput,
+        outputs: [new codepipeline.Artifact('ProdTemplate')],
+        runOrder: 1,
+    });
+
     const betaDeployAction = new cp_actions.CloudFormationCreateUpdateStackAction({
         actionName: 'DeployBeta',
         stackName: 'BlueEyedSoulLambdaBetaStack',
@@ -60,6 +121,7 @@ constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         adminPermissions: true,
         parameterOverrides: {
             LambdaFunctionName: 'BlueEyedSoul-Beta',
+            LambdaFunctionCodeBucketName: 'blue-eyed-soul-lambda-code-beta',
         },
     });
 
@@ -75,6 +137,7 @@ constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         adminPermissions: true,
         parameterOverrides: {
             LambdaFunctionName: 'BlueEyedSoul-Prod',
+            LambdaFunctionCodeBucketName: 'blue-eyed-soul-lambda-code-prod',
         },
         runOrder:2 // ensure that it runs second
     });
@@ -97,11 +160,11 @@ constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         },
         {
           stageName: 'DeployBeta',
-          actions: [betaDeployAction],
+          actions: [uploadAction, betaDeployAction],
         },
           {
               stageName: 'ApproveAndDeployProd',
-              actions: [prodApprovalAction, prodDeployAction],
+              actions: [prodApprovalAction, uploadActionProd,prodDeployAction],
           },
       ],
     });
