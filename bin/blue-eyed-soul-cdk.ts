f4ac092e1be07d7d2a4259dba42298abcf503164
Cossette Perez <cperezcomp1@student.rccd.edu>
2025-04-20 14:19:38 -0700

    Adding CDK for schedulers.

--- bin/blue-eyed-soul-cdk.ts
+++ bin/blue-eyed-soul-cdk.ts
@@ -2,10 +2,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { BlueEyedSoulPipelineStack } from '../lib/blue-eyed-soul-cdk-pipeline';
 import {BlueEyedCodeS3BucketsStack} from "../lib/blue-eyed-code-s3-stack";
+import {BlueEyedSoulSchedulerStack} from "../lib/blue-eyed-sould-scheduler-stack";
 
 const app = new cdk.App();
-new BlueEyedSoulPipelineStack(app, 'BlueEyedSoulLambdaPipeline', {
-});
+new BlueEyedSoulPipelineStack(app, 'BlueEyedSoulLambdaPipeline', {});
 
-new BlueEyedCodeS3BucketsStack(app, 'BlueEyedCodeS3BucketsStack', {
-})
+new BlueEyedCodeS3BucketsStack(app, 'BlueEyedCodeS3BucketsStack', {})
+
+new BlueEyedSoulSchedulerStack(app, 'BlueEyedSoulSchedulerStack', {})
---
+++ lib/blue-eyed-sould-scheduler-stack.ts
@@ -0,0 +1,56 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as scheduler from 'aws-cdk-lib/aws-scheduler';
+
+export class BlueEyedSoulSchedulerStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const lambdaArn = 'arn:aws:lambda:us-west-2:276366037431:function:BlueEyedSoul-Prod';
+
+        // IAM role for EventBridge Scheduler
+        const schedulerRole = new iam.Role(this, 'SchedulerRole', {
+            assumedBy: new iam.ServicePrincipal('scheduler.amazonaws.com'),
+        });
+
+        schedulerRole.addToPolicy(new iam.PolicyStatement({
+            actions: ['lambda:InvokeFunction'],
+            resources: [lambdaArn],
+        }));
+
+        // EventBridge Scheduler
+        new scheduler.CfnSchedule(this, 'TokenCheckSchedule-Prod', {
+            name: 'TokenCheckSchedule-Prod',
+            scheduleExpressionTimezone: 'America/Los_Angeles',
+            scheduleExpression: 'rate(5 minutes)',
+            flexibleTimeWindow: {
+                mode: 'OFF',
+            },
+            target: {
+                arn: lambdaArn,
+                roleArn: schedulerRole.roleArn,
+                input: JSON.stringify({ operation: 'TokenCheck' }),
+            },
+            description: 'Triggers BlueEyedSoul-Prod every 5 minutes',
+            state: 'ENABLED',
+        });
+
+        // EventBridge Scheduler
+        new scheduler.CfnSchedule(this, 'DailyCheckSchedule-Prod', {
+            name: 'DailyCheckSchedule-Prod',
+            description: 'Runs daily at 4:20 PM Pacific Time',
+            scheduleExpression: 'cron(20 16 * * ? *)',
+            scheduleExpressionTimezone: 'America/Los_Angeles',
+            flexibleTimeWindow: {
+                mode: 'OFF',
+            },
+            target: {
+                arn: lambdaArn,
+                roleArn: schedulerRole.roleArn,
+                input: JSON.stringify({ operation: 'TokenCheck' }),
+            },
+            state: 'ENABLED',
+        });
+    }
+}
