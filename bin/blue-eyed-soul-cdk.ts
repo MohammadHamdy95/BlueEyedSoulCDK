d4ff50bd8fa7ca6e85e468fd4da44b64e6ccf80c
Cossette Perez <cperezcomp1@student.rccd.edu>
2025-06-08 23:49:09 -0700

    making faster build times

--- bin/blue-eyed-soul-cdk.ts
+++ bin/blue-eyed-soul-cdk.ts
@@ -3,6 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { BlueEyedSoulPipelineStack } from '../lib/blue-eyed-soul-cdk-pipeline';
 import {BlueEyedCodeS3BucketsStack} from "../lib/blue-eyed-code-s3-stack";
 import {BlueEyedSoulSchedulerStack} from "../lib/blue-eyed-sould-scheduler-stack";
+import {ProcessingLambdaSchedulerStack} from "../lib/processing-lambda-scheduler-stack";
 
 const app = new cdk.App();
 new BlueEyedSoulPipelineStack(app, 'BlueEyedSoulLambdaPipeline', {});
@@ -10,3 +11,5 @@ new BlueEyedSoulPipelineStack(app, 'BlueEyedSoulLambdaPipeline', {});
 new BlueEyedCodeS3BucketsStack(app, 'BlueEyedCodeS3BucketsStack', {})
 
 new BlueEyedSoulSchedulerStack(app, 'BlueEyedSoulSchedulerStack', {})
+
+new ProcessingLambdaSchedulerStack(app, 'ProcessingLambdaSchedulerStack', {})
---
+++ lib/processing-lambda-scheduler-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as scheduler from 'aws-cdk-lib/aws-scheduler';
+
+export class ProcessingLambdaSchedulerStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const lambdaArn = 'arn:aws:lambda:us-west-2:276366037431:function:proccessing-lambda-stack-ProcessingLambda-6pvXhhTImkDv';
+
+        // IAM role for EventBridge Scheduler
+        const schedulerRole = new iam.Role(this, 'ProcessingSchedulerRole', {
+            assumedBy: new iam.ServicePrincipal('scheduler.amazonaws.com'),
+        });
+
+        schedulerRole.addToPolicy(new iam.PolicyStatement({
+            actions: ['lambda:InvokeFunction'],
+            resources: [lambdaArn],
+        }));
+
+        // Example: Every 5 minutes task
+        new scheduler.CfnSchedule(this, 'ProcessingCheckSchedule', {
+            name: 'ProcessingCheckSchedule',
+            scheduleExpressionTimezone: 'America/Los_Angeles',
+            scheduleExpression: 'rate(5 minutes)',
+            flexibleTimeWindow: {
+                mode: 'OFF',
+            },
+            target: {
+                arn: lambdaArn,
+                roleArn: schedulerRole.roleArn,
+                input: JSON.stringify({ operation: 'RefreshQueue' }), // customize as needed
+            },
+            description: 'Invokes Processing Lambda every 5 minutes',
+            state: 'ENABLED',
+        });
+    }
+}
