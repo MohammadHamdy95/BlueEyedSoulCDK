fba9f9d8676f2c49fc94a91c233b2c19dde6fe6e
Cossette Perez <cperezcomp1@student.rccd.edu>
2025-06-10 03:49:54 -0700

    making faster build times

--- lib/processing-lambda-scheduler-stack.ts
+++ lib/processing-lambda-scheduler-stack.ts
@@ -7,7 +7,7 @@ export class ProcessingLambdaSchedulerStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const lambdaArn = 'arn:aws:lambda:us-west-2:276366037431:function:proccessing-lambda-stack-ProcessingLambda-6pvXhhTImkDv';
+        const lambdaArn = 'arn:aws:lambda:us-west-2:276366037431:function:ProcessingLambda';
 
         // IAM role for EventBridge Scheduler
         const schedulerRole = new iam.Role(this, 'ProcessingSchedulerRole', {
