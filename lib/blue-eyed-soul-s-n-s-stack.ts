31fadf478d4068ab0127b1b571e78da963d333a3
Cossette Perez <cperezcomp1@student.rccd.edu>
2025-06-28 22:28:01 -0700

    ddb tables to cdk

--- lib/blue-eyed-soul-s-n-s-stack.ts
+++ lib/blue-eyed-soul-s-n-s-stack.ts
@@ -1,5 +1,9 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
-import { aws_sns as sns } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Duration } from 'aws-cdk-lib';
+import {
+    aws_sns as sns,
+    aws_sqs as sqs,
+    aws_sns_subscriptions as subs
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class BlueEyedSoulSNSStack extends Stack {
@@ -8,10 +12,24 @@ export class BlueEyedSoulSNSStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        // Create SNS topic
         this.cfSystemsUpdatesTopic = new sns.Topic(this, 'CFSystemsUpdatesTopic', {
             topicName: 'CFSystemsUpdatesTopic'
         });
 
+        // Create SQS queue to receive messages from the topic
+        const cfSystemsUpdatesQueue = new sqs.Queue(this, 'CFSystemsUpdatesQueue', {
+            queueName: 'CFSystemsUpdatesQueue',
+            visibilityTimeout: Duration.seconds(30),
+            retentionPeriod: Duration.days(4),
+        });
+
+        // Subscribe the queue to the SNS topic
+        this.cfSystemsUpdatesTopic.addSubscription(
+            new subs.SqsSubscription(cfSystemsUpdatesQueue)
+        );
+
+        // Export the topic ARN for use in SAM or other stacks
         new CfnOutput(this, 'CFSystemsTopicArn', {
             value: this.cfSystemsUpdatesTopic.topicArn,
             exportName: 'CFSystemsUpdatesTopicArn'
