d8e3e76921ac94edfedd7d8a0661ac554b023713
Mo Hamdy <moham1255@gmail.com>
2025-06-29 01:03:05 -0700

    ddb tables to cdk

--- lib/blue-eyed-soul-s-n-s-stack.ts
+++ lib/blue-eyed-soul-s-n-s-stack.ts
@@ -8,31 +8,69 @@ import { Construct } from 'constructs';
 
 export class BlueEyedSoulSNSStack extends Stack {
     public readonly cfSystemsUpdatesTopic: sns.Topic;
+    public readonly processingLambdaUpdatesTopic: sns.Topic;
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        // Create SNS topic
+        // === First SNS & SQS Setup ===
         this.cfSystemsUpdatesTopic = new sns.Topic(this, 'CFSystemsUpdatesTopic', {
             topicName: 'CFSystemsUpdatesTopic'
         });
 
-        // Create SQS queue to receive messages from the topic
         const cfSystemsUpdatesQueue = new sqs.Queue(this, 'CFSystemsUpdatesQueue', {
             queueName: 'CFSystemsUpdatesQueue',
             visibilityTimeout: Duration.seconds(30),
             retentionPeriod: Duration.days(4),
         });
 
-        // Subscribe the queue to the SNS topic
         this.cfSystemsUpdatesTopic.addSubscription(
             new subs.SqsSubscription(cfSystemsUpdatesQueue)
         );
 
-        // Export the topic ARN for use in SAM or other stacks
         new CfnOutput(this, 'CFSystemsTopicArn', {
             value: this.cfSystemsUpdatesTopic.topicArn,
             exportName: 'CFSystemsUpdatesTopicArn'
+        });
+
+        new CfnOutput(this, 'CFSystemsUpdatesQueueArn', {
+            value: cfSystemsUpdatesQueue.queueArn,
+            exportName: 'CFSystemsUpdatesQueueArn'
+        });
+
+        new CfnOutput(this, 'CFSystemsUpdatesQueueUrl', {
+            value: cfSystemsUpdatesQueue.queueUrl,
+            exportName: 'CFSystemsUpdatesQueueUrl'
+        });
+
+        // === New SNS & SQS Setup ===
+        this.processingLambdaUpdatesTopic = new sns.Topic(this, 'ProcessingLambdaUpdatesTopic', {
+            topicName: 'ProcessingLambdaUpdates'
+        });
+
+        const blueEyedSoulInboundUserChangesQueue = new sqs.Queue(this, 'BlueEyedSoulInboundUserChangesQueue', {
+            queueName: 'BlueEyedSoulInboundUserChanges',
+            visibilityTimeout: Duration.seconds(30),
+            retentionPeriod: Duration.days(4),
+        });
+
+        this.processingLambdaUpdatesTopic.addSubscription(
+            new subs.SqsSubscription(blueEyedSoulInboundUserChangesQueue)
+        );
+
+        new CfnOutput(this, 'ProcessingLambdaUpdatesTopicArn', {
+            value: this.processingLambdaUpdatesTopic.topicArn,
+            exportName: 'ProcessingLambdaUpdatesTopicArn'
+        });
+
+        new CfnOutput(this, 'BlueEyedSoulInboundUserChangesQueueArn', {
+            value: blueEyedSoulInboundUserChangesQueue.queueArn,
+            exportName: 'BlueEyedSoulInboundUserChangesQueueArn'
+        });
+
+        new CfnOutput(this, 'BlueEyedSoulInboundUserChangesQueueUrl', {
+            value: blueEyedSoulInboundUserChangesQueue.queueUrl,
+            exportName: 'BlueEyedSoulInboundUserChangesQueueUrl'
         });
     }
 }
